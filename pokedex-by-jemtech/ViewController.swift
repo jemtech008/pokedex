acbd93c513b14824cd0cd42d5a6a47a6f82ea75b
js <js@mobility.exodia.com>
2016-07-14 19:45:51 +0800

    Initial Commit

---
+++ pokedex-by-jemtech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-jemtech
+//
+//  Created by jemson sentillas on 14/07/2016.
+//  Copyright © 2016 jemson sentillas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
